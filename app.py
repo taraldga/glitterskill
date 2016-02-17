00d8a617e45b31de80c8370c40ad5ec9adc7bd19
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2016-02-17 12:19:52 +0100

    created template folder and first template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import render_template
 app = Flask(__name__)
 
 #This is an early version of the backend powering glitterskill
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+  return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
